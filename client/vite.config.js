bef589571278eebc236edb94725dac920fa8f690
Ivana Simic <isimic@extensionengine.com>
2025-12-02 14:56:00 +0100

    Add Tailwind

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), tailwindcss()],
   server: {
     port: 8080,
     proxy: {
       '/api': 'http://localhost:3000',
-  }}
+  }},
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 });
