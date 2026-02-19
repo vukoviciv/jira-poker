f4c191084b45dca46926019c1c66d26439a3ab7e
Ivana Simic <isimic@extensionengine.com>
2026-02-19 11:52:22 +0100

    Fix showing player joining the room

--- client/src/components/common/Snackbar.vue
+++ client/src/components/common/Snackbar.vue
@@ -1,6 +1,5 @@
 <template>
     <div
-      v-if="message" 
       id="snackbar" 
       class="bg-[#1e2a3a] border-1 border-cyan-500 text-cyan-400 p-4 rounded-md fixed top-19 left-1/2 -translate-x-1/2 min-w-[250px] max-w-[90%] w-fit z-[9999] shadow-lg">
     {{ message }}
@@ -9,7 +8,7 @@
 
 <script setup>
 defineProps({
-  message: { type: String, required: true }
+  message: { type: String, default: '' }
 })
 </script>
 
